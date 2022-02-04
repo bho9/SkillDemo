7973d6d11e86f4845ddb020bf4fe61ba187ec295
bho9 <97492583+bho9@users.noreply.github.com>
2022-02-04 14:36:32 -0800

    Added jar and test files.

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest
+{
+    @Test
+    public void squareTest1()
+    {
+        assertEquals(4, SkillDemo.square(4));
+    }
+}
